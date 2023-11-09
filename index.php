bdf391c86240c3c4ef2793725c3ca7976d738188
Levin Aleksey <levin.penza@yandex.ru>
2023-11-09 22:51:41 +0300

    Копирование проекта на Ubuntu

--- index.php
+++ index.php
@@ -7,9 +7,9 @@ $user_name = 'Alex'; // укажите здесь ваше имя
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title>Главная</title>
-    <link href="../css/normalize.min.css" rel="stylesheet">
-    <link href="../css/style.css" rel="stylesheet">
+    <title>Главная!</title>
+    <link href="./css/normalize.min.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
 </head>
 <body>
 <div class="page-wrapper">
@@ -18,7 +18,7 @@ $user_name = 'Alex'; // укажите здесь ваше имя
     <div class="main-header__container container">
         <h1 class="visually-hidden">YetiCave</h1>
         <a class="main-header__logo">
-            <img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
+            <img src="./img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
         </a>
         <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
             <input type="search" name="search" placeholder="Поиск лота">
